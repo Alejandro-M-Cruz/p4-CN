d511de056ef99ce367461640ac7a82ac55b71781
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-11-25 22:36:20 +0000

    Minor indentation change

--- push_to_erc.sh
+++ push_to_erc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 989870301024.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 |
+  docker login --username AWS --password-stdin 989870301024.dkr.ecr.us-east-1.amazonaws.com
 
 if [ $# -gt 1 ]
 then
